b75945e9c4856e97eb88bcc5355ffbe7f8c4d5dc
Marco-Senzamici <95362851+Marco-Senzamici@users.noreply.github.com>
2022-03-04 17:16:16 +0100

    Create ApiEventbriteApplicationTests.java

---
+++ api-eventbrite/src/test/java/it/univpm/apieventbrite/ApiEventbriteApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.apieventbrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEventbriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
