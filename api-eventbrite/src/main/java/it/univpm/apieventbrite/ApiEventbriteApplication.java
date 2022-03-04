88e3dea44da25318351738a17dbb9014c6e45348
Marco-Senzamici <95362851+Marco-Senzamici@users.noreply.github.com>
2022-03-04 17:16:10 +0100

    Create ApiEventbriteApplication.java

---
+++ api-eventbrite/src/main/java/it/univpm/apieventbrite/ApiEventbriteApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.apieventbrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEventbriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEventbriteApplication.class, args);
+	}
+
+}
